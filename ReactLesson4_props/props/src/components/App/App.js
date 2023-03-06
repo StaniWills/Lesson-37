3f1fdc34703772c2bfd0e79c59354f99c1f69af8
StaniWills <deberkader@mail.ru>
2023-03-06 10:48:19 +0100

    propsStart

---
+++ ReactLesson4_props/props/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Block from '../Block/Block'
+
+function App() {
+  return (
+    <div>
+        <Block title='Title 1' post={10}/>
+    </div>
+  )
+}
+
+export default App
---
+++ ReactLesson4_props/props/src/components/Block/Block.js
@@ -0,0 +1,12 @@
+
+function Block(props) {
+    console.log(props);
+    return (
+      <div>
+        <h2>Title Post</h2>
+        <p>Text Post</p>
+      </div>
+    )
+  }
+  
+  export default Block
